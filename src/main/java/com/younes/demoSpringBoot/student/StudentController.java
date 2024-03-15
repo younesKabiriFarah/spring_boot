19fb68da1f9ad0278a72306e364799fe6a8e5ca1
younesKabiriFarah <youneskabiri17@gmial.com>
2024-03-15 21:36:58 +0100

    Our CRUD implemented

--- src/main/java/com/younes/demoSpringBoot/student/StudentController.java
+++ src/main/java/com/younes/demoSpringBoot/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.younes.demoSpringBoot.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,13 +12,42 @@ public class StudentController {
 
 
     private StudentService service;
-    
-    public StudentController(StudentService service){
+
+    public StudentController(StudentService service) {
         this.service = service;
     }
 
+    @PostMapping
+    public Student save(
+            @RequestBody Student student
+    ) {
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(
+            @PathVariable String email
+    ) {
+        return service.findByEmail(email);
+    }
+
     @GetMapping
-    public List<Student> findAllStudents(){
+    public List<Student> findAllStudents() {
         return service.findAllStudents();
     }
+
+    @PutMapping
+    public Student updateStudent(
+            @RequestBody Student student
+    ) {
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete(
+            @PathVariable("email") String email
+    ) {
+        service.delete(email);
+    }
+
 }
--- src/main/java/com/younes/demoSpringBoot/student/StudentService.java
+++ src/main/java/com/younes/demoSpringBoot/student/StudentService.java
@@ -7,35 +7,14 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents (){
-        return List.of(
-                new Student(
-                        "Younes",
-                        "Kabiri",
-                        LocalDate.now(),
-                        "younes@gmail.com",
-                        34
+public interface StudentService {
+    Student save(Student s);
 
-                ),
-                new Student(
-                        "Joel",
-                        "Panisello",
-                        LocalDate.now(),
-                        "joel@gmail.com",
-                        30
+    List<Student> findAllStudents ();
 
-                ),
-                new Student(
-                        "Arey",
-                        "Ferrero",
-                        LocalDate.now(),
-                        "arey@gmail.com",
-                        36
+    Student findByEmail(String email);
 
-                )
+    Student update(Student s);
 
-        );
-    }
+    void delete(String email);
 }
